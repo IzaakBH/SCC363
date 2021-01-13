7813c9d2be464e1bfa26ea11b60bd866ee72b7d2
Izaak Berkeley-Hurst <iberkeleyhurst@gmail.com>
2021-01-13 21:17:52 +0000

    Initial Comit

---
+++ src/test/java/com/scc363/hospitalsystem/HospitalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scc363.hospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
