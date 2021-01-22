cec33f0e0b2c8448c092de3c3306fe31c5eb6fd3
Izaak Berkeley-Hurst <iberkeleyhurst@gmail.com>
2021-01-22 19:32:58 +0000

    SCC363-27 Implement Spring Login
    Implemented basic site-wide authentication as per tutorial https://developer.okta.com/blog/2019/05/15/spring-boot-login-options

---
+++ src/main/java/com/scc363/hospitalproject/security/LoginSecurity.java
@@ -0,0 +1,22 @@
+package com.scc363.hospitalproject.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class LoginSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // Tells spring to authorise all requests with basic authentication
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    // Temp method to create a user with username user password pass and role USER
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Password storage format is specified as required in spring security 5 and above
+        auth.inMemoryAuthentication().withUser("user").password("{noop}pass").roles("USER");
+    }
+}
