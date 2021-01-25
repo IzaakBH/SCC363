5c1d7f481d90cf32109e5746a33e678b130117ef
mntemiri <48097194+mntemiri@users.noreply.github.com>
2021-01-25 08:32:15 +0000

    Random added in CodeGen.java

--- src/main/java/com/scc363/hospitalproject/services/CodeGen.java
+++ src/main/java/com/scc363/hospitalproject/services/CodeGen.java
@@ -1,9 +1,13 @@
 package com.scc363.hospitalproject.services;
 
+import java.util.Random;
+
 public class CodeGen {
+
+    Random rand = new Random();
 
     //It returns a randomly generated 4 digits number
     public int generateCode() {
-        return rand.nextInt((9999 - 100) + 1) + 10;;
+        return rand.nextInt((9999 - 100) + 1) + 10;
     }
 }
