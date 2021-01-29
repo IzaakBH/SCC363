64c63e8ff26767af258483d6926b700a36860d43
Izaak Berkeley-Hurst <iberkeleyhurst@gmail.com>
2021-01-29 19:08:03 +0000

    SCC363-48 Hashing on authentication
    Fixed login logic

--- src/main/java/com/scc363/hospitalproject/security/LoginSecurity.java
+++ src/main/java/com/scc363/hospitalproject/security/LoginSecurity.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -24,17 +25,14 @@ public class LoginSecurity extends WebSecurityConfigurerAdapter {
         // Tells spring to authorise all requests with basic authentication
         http.csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/h2-console/**", "/h2-console/*", "/hello", "/signin").permitAll()
-                    .antMatchers("/css/**", "/js/**", "/img/**").permitAll()  //Permit access to static files
-                    .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/", "/home", "/register", "/verify-account", "/verifymessage", "/verified", "/notverified").permitAll()
+                    .antMatchers("/h2-console/**", "/h2-console/*", "/signin", "/hello").permitAll()
+                    .antMatchers("/js/**", "/css/**", "/img/**").permitAll()  //Permit access to static files
+                    .antMatchers("/test/**").hasRole("SYSADMIN")
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/signin")
-                    .loginProcessingUrl("/signin")   // Where we submit the username and password to
-                    .defaultSuccessUrl("/hello")
-                    .failureUrl("/register.html?error=true")
+                    .loginPage("/signin").permitAll()
                     .and()
                 .logout()
                     .permitAll();
@@ -43,21 +41,9 @@ public class LoginSecurity extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
     }
 
-    // Temp method to create a user with username user password pass and role USER
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Password storage format is specified as required in spring security 5 and above
-        // Change the encoder to custom hashing method.
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-        auth.authenticationProvider(authProvider());
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                    .password(passwordEncoder().encode("pass"))
-//                    .roles("USER")
-//                .and()
-//                    .withUser("admin")
-//                    .password(passwordEncoder().encode("pass"))
-//                    .roles("ADMIN");
     }
 
     @Bean
@@ -72,4 +58,5 @@ public class LoginSecurity extends WebSecurityConfigurerAdapter {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
+
 }
--- src/main/java/com/scc363/hospitalproject/services/UserDetailsServiceImpl.java
+++ src/main/java/com/scc363/hospitalproject/services/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import com.scc363.hospitalproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,11 +22,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository repo;
 
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repo.findUserByEmail(email);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findUserByUsername(username);
 
         if (user == null) {
-            throw new UsernameNotFoundException("No user exists with the email " + email);
+            throw new UsernameNotFoundException("Error logging in");
+        } else {
+
         }
         return user;
     }
