4c2e2a6d44699797fd18f6e7a9597cbf5d84a87f
mntemiri <48097194+mntemiri@users.noreply.github.com>
2021-02-07 16:22:16 +0100

    Fixed the bug that forced the program to stop

---
+++ src/main/java/com/scc363/hospitalproject/datamodels/Log.java
@@ -0,0 +1,41 @@
+package com.scc363.hospitalproject.datamodels;
+
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Entity
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer errorId;
+
+    @Column
+    private LocalDateTime date;
+
+    @Column
+    private String error;
+
+    @Column
+    private String level;
+
+    @Column
+    private String message;
+
+    @Column
+    private String userName;
+
+    public Log(LocalDateTime date, String level, String message, String userName){
+        this.date = date;
+        this.level = level;
+        this.message = message;
+        this.userName = userName;
+        this.error = "Error";
+    }
+
+}
