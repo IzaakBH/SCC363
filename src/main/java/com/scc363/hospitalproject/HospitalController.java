4b5f8716d3f095031a1c9d6a164cb9c3b0df9b76
xavier hickman <xavierhickman1234@gmail.com>
2021-02-11 23:00:30 +0000

    implemented update user password service

--- src/main/java/com/scc363/hospitalproject/HospitalController.java
+++ src/main/java/com/scc363/hospitalproject/HospitalController.java
@@ -14,6 +14,7 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
